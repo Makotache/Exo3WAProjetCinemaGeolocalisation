3f030231eb22353eb015365edd1600c14ad43952
Makotache <victorcullet@gmail.com>
2023-10-06 12:05:52 +0200

    fin de geolocalisation

---
+++ js/mainGeolocalisation.js
@@ -0,0 +1,24 @@
+import {haversine} from "./utils/math.js"
+
+function GetPosition()
+{
+    return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(position => 
+        {
+            resolve(position.coords);
+        });
+    });
+}
+
+let userPos = {};
+await GetPosition().then(pos => 
+{
+    userPos.latitude = pos.latitude;
+    userPos.longitude = pos.longitude;
+});
+console.log(userPos);
+
+export function GetCinemaDistance(cinemaPos)
+{
+    return haversine([userPos.latitude, userPos.longitude], [cinemaPos.geolocalisation.lat, cinemaPos.geolocalisation.lon])
+}
